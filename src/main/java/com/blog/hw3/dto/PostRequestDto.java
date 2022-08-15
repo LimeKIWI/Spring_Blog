aedae25a196821d8f5dbb8949f65878c7da9242d
limeKIWI <pop78978@gmail.com>
2022-08-15 19:47:03 +0900

    repo, entity, dto 틀 작성, 게시글 작성시간을 위해 timestamped.java 추가, h2db를 이용하기 위해서 h2접근구문 추가 #3

--- src/main/java/com/blog/hw3/dto/PostRequestDto.java
+++ src/main/java/com/blog/hw3/dto/PostRequestDto.java
@@ -1,4 +1,11 @@
 package com.blog.hw3.dto;
 
+import lombok.Getter;
+
+@Getter
 public class PostRequestDto {
+    private String title;
+    private String author;
+    private String content;
+    private String password;
 }
