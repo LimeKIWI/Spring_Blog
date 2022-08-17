51e5741f88bc942102f5d630704f5d42927dba3e
limeKIWI <pop78978@gmail.com>
2022-08-17 21:11:59 +0900

    테이블을 나누는것이 비효율적이므로 삭제후 전체조회와 글조회를 위한 dto를 새로작성. 생성자를 이용하여 dto를 만드는것과 builder를 이용하여 dto를 만드는 방식을 둘다 사용해봄.

--- src/main/java/com/blog/hw3/service/PostService.java
+++ src/main/java/com/blog/hw3/service/PostService.java
@@ -1,16 +1,17 @@
 package com.blog.hw3.service;
 
+import com.blog.hw3.dto.DetailPostDto;
 import com.blog.hw3.dto.PasswordDto;
+import com.blog.hw3.dto.PostListDto;
 import com.blog.hw3.dto.PostRequestDto;
-import com.blog.hw3.entity.Password;
 import com.blog.hw3.entity.Post;
-import com.blog.hw3.repository.PasswordRepository;
 import com.blog.hw3.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,50 +20,57 @@ import java.util.List;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final PasswordRepository passwordRepository;
 
     //글업데이트
     @Transactional
-    public Post update(Long id, PostRequestDto requestDto) {
+    public DetailPostDto update(Long id, PostRequestDto requestDto) {
         Post post = postRepository.findById(id).orElseThrow(() -> new NullPointerException("아이디가 존재하지 않습니다."));
-        Password pw = passwordRepository.findById(id).orElseThrow(() -> new NullPointerException("아이디가 존재하지 않습니다."));
         post.update(requestDto);
-        pw.update(requestDto);
-        return post;
+        return new DetailPostDto(post);
+
     }
 
     //글저장
     @Transactional
-    public Post create(PostRequestDto requestDto) {
+    public DetailPostDto create(PostRequestDto requestDto) {
         Post post = new Post(requestDto);
-        Password pw = new Password(requestDto);
         postRepository.save(post);
-        passwordRepository.save(pw);
-        return post;
+        return new DetailPostDto(post);
     }
 
     //글삭제
     @Transactional
     public void delete(Long id) {
         postRepository.deleteById(id);
-        passwordRepository.deleteById(id);
     }
 
     //글상세보기
-    public Post getDetailPost(long id) {
-        return postRepository.findById(id).orElseThrow(() -> new NullPointerException("해당 글이 존재하지 않습니다."));
+    public DetailPostDto getDetailPost(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new NullPointerException("해당 글이 존재하지 않습니다."));
+        return new DetailPostDto(post);
     }
 
-    //글목록조회
-    public List<Post> getPosts() {
-        return postRepository.findAllByOrderByCreateAtDesc();
+    //글목록보기
+    public List<PostListDto> getPosts() {
+        List<Post> list = postRepository.findAllByOrderByCreateAtDesc();
+        List<PostListDto> plist = new ArrayList<>();
+        for(Post post : list){
+            PostListDto listDto = PostListDto.builder()
+                    .id(post.getId())
+                    .title(post.getTitle())
+                    .author(post.getAuthor())
+                    .createAt(post.getCreateAt())
+                    .modifiedAt(post.getModifiedAt())
+                    .build();
+            plist.add(listDto);
+        }
+        return plist;
     }
 
     //비밀번호 확인
     public Boolean chkPassword(Long id, PasswordDto requestDto) {
-        Password pw = passwordRepository.findById(id).orElseThrow(() -> new NullPointerException("아이디가 존재하지 않습니다."));
-        return pw.getPassword().equals(requestDto.getPassword());
+        Post post = postRepository.findById(id).orElseThrow(() -> new NullPointerException("아이디가 존재하지 않습니다."));
+        return post.getPassword().equals(requestDto.getPassword());
     }
-
 
 }
