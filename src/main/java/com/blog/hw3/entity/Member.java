93c030ebb521414bb6c8a7095c739f4291182971
limeKIWI <pop78978@gmail.com>
2022-08-25 16:02:20 +0900

    기본키 자동생성방식을 각자 카운트하는 방식으로 변경

--- src/main/java/com/blog/hw3/entity/Member.java
+++ src/main/java/com/blog/hw3/entity/Member.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Getter
 public class Member {
 
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
